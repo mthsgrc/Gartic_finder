cab392c5ff114d6a5bee156cce75a40b58c00b48
mthsgrc <matheus.dev.garcia@gmail.com>
2022-09-20 15:22:07 -0300

    Clean function on EventListener in buttons

--- js/main.js
+++ js/main.js
@@ -98,44 +98,49 @@ let arrayWordList = [
     "vidro", "viela", "vigia", "vilão", "vinho", "viola", "vodca", "votar",
     "xexéu", "zebra", "zíper", "zumbi"
 ];
+
 const threeLetterBtn = document.querySelector(".btn-3");
 const fourLetterBtn = document.querySelector(".btn-4");
 const fiveLetterBtn = document.querySelector(".btn-5");
+const sixLetterBtn = document.querySelector(".btn-6");
+const sevenLetterBtn = document.querySelector(".btn-7");
+const eightLetterBtn = document.querySelector(".btn-8");
+const nineLetterBtn = document.querySelector(".btn-9");
+const tenLetterBtn = document.querySelector(".btn-10");
+const elevenLetterBtn = document.querySelector(".btn-11");
+const twelveLetterBtn = document.querySelector(".btn-12");
+const thirtLetterBtn = document.querySelector(".btn-13");
+const fourtLetterBtn = document.querySelector(".btn-14");
+const fivetLetterBtn = document.querySelector(".btn-15");
 
-threeLetterBtn.addEventListener("click", function() {
-    let threeLetters = wordLengthFilter( arrayWordList, 3);
-    printWord(threeLetters);
-});
+const wrapper = document.getElementById("wrapper");
 
-
-fourLetterBtn.addEventListener("click", function() {
-    let fourLetters = wordLengthFilter( arrayWordList, 4);
-    printWord(fourLetters);
-});
-
-
-fiveLetterBtn.addEventListener("click", function() {
-    let fiveLetters = wordLengthFilter( arrayWordList, 5);
-    printWord(fiveLetters);
-});
+wrapper.addEventListener("click", (event) => {
+	const isButton = event.target.nodeName === "BUTTON";
+	if(!isButton) {
+		return;
+	}
+	let wordLengthValue = parseInt(event.target.value, 10);
+	let nLettersWords = wordLengthFilter(arrayWordList, wordLengthValue);
+	printWords(nLettersWords);
+})
 
 function wordLengthFilter(words, i) {
     filteredWords = words.filter(word => word.length == i);
     return filteredWords;
 }
 
-function printWord(words) {
+function printWords(words) {
     const printedWordListDiv = document.querySelector(".printedWordList");
     let text = "";
     for (let i = 0; i < words.length; i++) {
-        text += " - " + words[i] + " ";
+    		i == 0 ? text += words[i] + " " : text += " - " + words[i] + " ";
     }
     text += ""
     printedWordListDiv.innerHTML = text;
 }
-printWord(arrayWordList);
-let wordLength = 5;
-let consultLetter = new Array(wordLength);
+
+printWords(arrayWordList);
 
 
 
