fb4be38614ab7ae78c453106ab8b133fcbb84735
mthsgrc <matheus.dev.garcia@gmail.com>
2022-09-21 23:09:49 -0300

    Working filter words by length

---
+++ js/master.js
@@ -0,0 +1,13 @@
+function include(file) {
+  
+  var script  = document.createElement('script');
+  script.src  = file;
+  // script.type = 'text/javascript';
+  // script.defer = true;
+  
+  document.getElementsByTagName('body').item(0).appendChild(script);
+  
+}
+
+include("js/words.js");
+include("js/main.js");
