85c56c5d4d078e62dbb82bd8043125ec9fab8bd8
mthsgrc <matheus.dev.garcia@gmail.com>
2022-09-27 17:03:32 -0300

    sync

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-const threeLetterBtn = document.querySelector(".btn-3");
-const fourLetterBtn = document.querySelector(".btn-4");
-const fiveLetterBtn = document.querySelector(".btn-5");
-const sixLetterBtn = document.querySelector(".btn-6");
-const sevenLetterBtn = document.querySelector(".btn-7");
-const eightLetterBtn = document.querySelector(".btn-8");
-const nineLetterBtn = document.querySelector(".btn-9");
-const tenLetterBtn = document.querySelector(".btn-10");
-const elevenLetterBtn = document.querySelector(".btn-11");
-const twelveLetterBtn = document.querySelector(".btn-12");
-const thirtLetterBtn = document.querySelector(".btn-13");
-const fourtLetterBtn = document.querySelector(".btn-14");
-const fivetLetterBtn = document.querySelector(".btn-15");
+"use strict";
+
+// const threeLetterBtn = document.querySelector(".btn-3");
+// const fourLetterBtn = document.querySelector(".btn-4");
+// const fiveLetterBtn = document.querySelector(".btn-5");
+// const sixLetterBtn = document.querySelector(".btn-6");
+// const sevenLetterBtn = document.querySelector(".btn-7");
+// const eightLetterBtn = document.querySelector(".btn-8");
+// const nineLetterBtn = document.querySelector(".btn-9");
+// const tenLetterBtn = document.querySelector(".btn-10");
+// const elevenLetterBtn = document.querySelector(".btn-11");
+// const twelveLetterBtn = document.querySelector(".btn-12");
+// const thirtLetterBtn = document.querySelector(".btn-13");
+// const fourtLetterBtn = document.querySelector(".btn-14");
+// const fivetLetterBtn = document.querySelector(".btn-15");
+
+const nLetterBtn = document.querySelector(".filter-btn");
 const alltLetterBtn = document.querySelector(".btn-all");
 const inputWord = document.querySelector(".input-field");
 const wrapper = document.getElementById("wrapper");
@@ -21,8 +25,11 @@ alltLetterBtn.addEventListener("click", (event) => {
   if (!isAllBtn) {
     return;
   }
+  wordLengthValue = 24;
+  inputWord.value === "" ? printWords(arrayWordList) : getWord(inputWord.value);
+
   printWords(arrayWordList);
-})
+});
 
 wrapper.addEventListener("click", (event) => {
   const isButton = event.target.nodeName === "BUTTON";
@@ -33,8 +40,8 @@ wrapper.addEventListener("click", (event) => {
   }
   wordLengthValue = parseInt(event.target.value, 10);
   let nLettersWords = wordLengthFilter(arrayWordList, wordLengthValue);
-  inputWord.value == "" ? printWords(nLettersWords) : getWord(inputWord.value);
-})
+  inputWord.value === "" ? printWords(nLettersWords) : getWord(inputWord.value);
+});
 
 function wordLengthFilter(words, i) {
   let filteredWords = words.filter(word => word.replace(/ /g, "").length == i);
@@ -45,9 +52,9 @@ function printWords(words) {
   const printedWordListDiv = document.querySelector(".printedWordList");
   let text = "";
   for (let i = 0; i < words.length; i++) {
-    i == 0 ? text += words[i] + " " : text += " - " + words[i] + " ";
+    i === 0 ? text += words[i] + " " : text += " - " + words[i] + " ";
   }
-  text += ""
+  text += "";
   printedWordListDiv.innerHTML = text;
 }
 
