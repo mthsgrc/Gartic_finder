3665d1ebdd77d590034d2bb8461e026a1bdf1c0f
mthsgrc <matheus.dev.garcia@gmail.com>
2022-10-06 15:07:03 -0300

    Clean files. Make filter word better

--- js/main.js
+++ js/main.js
@@ -6,55 +6,58 @@ const inputWord = document.querySelector(".input-field");
 const wrapper = document.getElementById("wrapper");
 let wordLengthValue = 0;
 
-alltLetterBtn.addEventListener("click", (event) => {
-  const isAllBtn = event.target.value === "all";
-  if (!isAllBtn) {
-    return;
-  }
-  wordLengthValue = 24;
-  inputWord.value === "" ? printWords(arrayWordList) : getWord(inputWord.value);
+wrapper.addEventListener("click", () => {
+	const isButton = event.target.nodeName === "BUTTON";
+	if (!isButton) {
+		return;
+	}
 
-  printWords(arrayWordList);
-});
-
-wrapper.addEventListener("click", (event) => {
-  const isButton = event.target.nodeName === "BUTTON";
-  if (!isButton) { return; }
-  if (event.target.value == "25") {
-    printWords(arrayWordList);
-    return;
-  }
-  wordLengthValue = parseInt(event.target.value, 10);
-  let nLettersWords = wordLengthFilter(arrayWordList, wordLengthValue);
-  inputWord.value === "" ? printWords(nLettersWords) : getWord(inputWord.value);
+	if (event.target.value == "all") {
+		printWords(arrayWordList);
+		return;
+	}
+	wordLengthValue = parseInt(event.target.value, 10);
+	let nLettersWords = wordLengthFilter(arrayWordList, wordLengthValue);
+	if (inputWord.value === "") {
+		printWords(nLettersWords);
+	} else {
+		getWord(inputWord.value);
+	}
 });
 
 function wordLengthFilter(words, i) {
-  let filteredWords = words.filter(word => word.replace(/ /g, "").length == i);
-  return filteredWords;
-}
-
-function printWords(words) {
-  const printedWordListDiv = document.querySelector(".printedWordList");
-  let text = "";
-  for (let i = 0; i < words.length; i++) {
-    i === 0 ? text += words[i] + " " : text += " - " + words[i] + " ";
-  }
-  text += "";
-  printedWordListDiv.innerHTML = text;
+	let filteredWords = words.filter(word => word.replace(/ /g, "").length == i);
+	return filteredWords;
 }
 
 function getWord(word = inputWord.value) {
-  let tmpWordsArray = wordLengthFilter(arrayWordList, wordLengthValue);
-  let guessedWord = word.split("");
+	let tmpWordsArray = wordLengthFilter(arrayWordList, wordLengthValue);
+	let guessedWord = word.split("");
 
-  let possibleWords = [];
+	let possibleWords = [];
 
-  tmpWordsArray.filter((w) => {
-    if (w[0] === guessedWord[0]) possibleWords.push(w);
-  });
+	tmpWordsArray.filter((w) => {
+		let tmpRegex = guessedWord.join("").replace(/ /gi, ".");
+		let regexStr = new RegExp(`^${tmpRegex}`, 'g');
 
-  printWords(possibleWords);
+		if (w.match(regexStr) !== null) possibleWords.push(w);
+	});
+
+	printWords(possibleWords);
+}
+
+function printWords(words) {
+	const printedWordListDiv = document.querySelector(".printedWordList");
+	let text = "";
+	for (let i = 0; i < words.length; i++) {
+		if (i === 0) {
+			text += words[i] + " ";
+		} else {
+			text += " - " + words[i] + " ";
+		}
+	}
+	text += "";
+	printedWordListDiv.innerHTML = text;
 }
 
 printWords(arrayWordList);
